6864916cab90e633645fc04b3222f168b119088d
adrianMartinezTeba <adrymate@gmail.com>
2023-06-28 18:28:24 +0200

    estructura acabada

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,7 +6,9 @@ const Person = () => {
       <div className='cabeza-container'>
         <div className='cabeza-sup'>
           <div className='gorra1'></div>
-          <div className="gorra2"></div>
+          <div className="gorra2"><div className='gorra-name'>FULL-STACK DEVELOPER</div>
+            {/* <p>FULL-STACK DEVELOPER</p> */}
+          </div>
         </div>
         <div className='cabeza-inf'>
           <div className="ceja"></div>
@@ -17,19 +19,56 @@ const Person = () => {
           <div className="boca"></div>
         </div>
       </div>
+      <div className="cuello"></div>
       <div className='tronco-container'>
+       
+        <div className='brazo-izq'>
+          <div className="manga-izq"></div>
+          <div className="bi-sup"></div>
+          <div className="codo-izq"></div>
+          <div className="bi-inf"></div>
+          <div className="mano-izq">
+            <div className="dedo"></div>
+            <div className="dedo"></div>
+            <div className="dedo"></div>
+            <div className="dedo"></div>
+          </div>
+        </div>
         <div className='torso'></div>
-        <div className='brazo-izq'></div>
-        <div className='brazo-der'></div>
-
+        <div className='brazo-der'>
+          <div className="manga-der"></div>
+          <div className="bd-sup"></div>
+          <div className="codo-der"></div>
+          <div className="bd-inf"></div>
+          <div className="mano-der">
+            <div className="dedo"></div>
+            <div className="dedo"></div>
+            <div className="dedo"></div>
+            <div className="dedo"></div>
+          </div>
+        </div>
       </div>
       <div className='inferior-container'>
         <div className='pierna-izq'>
-          <div className='zapato-izq'></div>
+          <div className="masa-izq"></div>
+            <div className='zapato-izq'>
+          <div className='cordones'>
+              <div className="cordon"></div>
+              <div className="cordon"></div>
+              <div className="cordon"></div>
+            </div>
           </div>
+        </div>
         <div className='pierna-der'>
-          <div className='zapato-der'></div>
+          <div className="masa-der"></div>
+          <div className='zapato-der'>
+            <div className='cordones'>
+              <div className="cordon"></div>
+              <div className="cordon"></div>
+              <div className="cordon"></div>
+            </div>
           </div>
+        </div>
       </div>
     </div>
   )
--- src/components/Proyectos/Proyectos.jsx
+++ src/components/Proyectos/Proyectos.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Proyectos.scss'
+import Person from '../Person/Person';
 const Proyectos = () => {
   return (
     <div>
           {/* <button><Link to={'/'}>Home</Link></button>
       <p>proyectos</p> */}
-       <div className="person-container">
-      <div className="head"></div>
-      <div className="body"></div>
-      <div className="legs"></div>
-    </div>
+      <Person/>
     </div>
   )
 }
