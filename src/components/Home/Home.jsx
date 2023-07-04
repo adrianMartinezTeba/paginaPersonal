0d76d79d25fea9af4d1353eabaafc198da91014a
adrianMartinezTeba <adrymate@gmail.com>
2023-07-04 16:25:28 +0200

    haciendo el home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.scss';
-
+import Person from '../Person/Person';
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="buttons-container">
-        <button className="museo-button">
-          <Link to={'/proyectos'}>
-            <div className="museum-container">
-              <div className="building">
-                <div className="title-museum">My projects</div>
-                <div className="entrance">
-                  <div className="door"></div>
+      <div>
+        <div className="home-sup">
+          <div className="proy-div">
+            <Link to={'/proyectos'}>Proyectos</Link>
+            <div className="proy-home-container">
+              <div className="cartel-proy">Proyectos</div>
+              <div className="puerta-container">
+                <div className="puerta">
+                  <div className="pta-izq"></div>
+                  <div className="pta-der"></div>
                 </div>
-                <div className="windows">
-                  <div className="window"></div>
-                  <div className="window"></div>
-                  <div className="window"></div>
+                <div className="timbre-container">
+                  <div className="timbre"></div>
                 </div>
               </div>
             </div>
-          </Link>
-        </button>
-        <button className="biografia-button">
-          <Link to={'/biografia'}>Biografía</Link>
-        </button>
+          </div>
+          <div className='divPerson-home'>
+            <Person/>
+            </div>
+          <div className="bio-div">
+            <Link to={'/biografia'}>Biografía</Link>
+          </div>
+        </div>
+        <div className="home-inf"></div>
       </div>
-      <div className="person"></div>
     </div>
   );
 };
