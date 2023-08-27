c790061860bed1414dfc0d0b6e6bbce6c209121c
adrianMartinezTeba <adrymate@gmail.com>
2023-08-27 16:25:31 +0200

    añadidos dos proyectos a proyectos

--- src/components/Proyectos/Proyectos.jsx
+++ src/components/Proyectos/Proyectos.jsx
@@ -3,40 +3,61 @@ import './Proyectos.scss';
 import { useNavigate } from "react-router-dom";
 import Person from '../Person/Person';
 import { PersonContext } from '../../context/PersonContext/PersonState';
-import imagenCuadro from '../../assets/imagenes/imagenCuadro.jpg'
+import imagenCuadro from '../../assets/imagenes/imagenCuadro.jpg';
+import yo from '../../assets/imagenes/yo.jpeg';
+
 const Proyectos = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { positionProyectos, updatePositionHome, updatePositionHall } = useContext(PersonContext);
-  const [info, setInfo] = useState(false)
-  const paginasWeb =
-  {
-    img: imagenCuadro,
-    urlProyecto: 'https://portfolio-adrianmartinezteba.vercel.app/',
-    gitHubBackend: 'Sin backend',
-    gitHubFrontend: 'https://github.com/adrianMartinezTeba/paginaPersonal',
-    nombre: 'Portfolio',
-    descripcion: 'Mi portfolio personal',
-    tecnologías: ['React.js, framer-motion, node.js, SASS, HTML, CSS, JavaScript']
-  }
+  const [activeProjectIndex, setActiveProjectIndex] = useState(null);
+
+  const projects = [
+    {
+      img: imagenCuadro,
+      urlProyecto: 'https://portfolio-adrianmartinezteba.vercel.app/',
+      gitHubBackend: 'Sin backend.',
+      gitHubFrontend: 'https://github.com/adrianMartinezTeba/paginaPersonal',
+      nombre: 'Portfolio.',
+      descripcion: 'Mi portfolio personal.',
+      tecnologías: ['React.js, framer-motion, JavaScript(consulta su correspondiente github para más información).'],
+      estado: 'Acabado.'
+    },
+    {
+      img: yo,
+      urlProyecto: 'Próximamente.',
+      gitHubBackend: 'https://github.com/adrianMartinezTeba/Back-rutinAdri',
+      gitHubFrontend: 'https://github.com/adrianMartinezTeba/frontend-rutinAdri',
+      nombre: 'RutinAdri.',
+      descripcion: 'Una red social donde crear tus rutinas y ver las de los demás.',
+      tecnologías: ['React.js,MongoDB,JavaScript(consulta sus correspondientes githubs para más información).'],
+      estado: 'En desarrollo.'
+    },
+    {
+      img: yo,
+      urlProyecto: 'Próximamente.',
+      gitHubBackend: 'https://github.com/adrianMartinezTeba/backend-instAdri',
+      gitHubFrontend: 'https://github.com/adrianMartinezTeba/frontend-instAdri',
+      nombre: 'InstAdri.',
+      descripcion: 'Mi portfolio personal.',
+      tecnologías: ['React.js,MongoDB,JavaScript(consulta sus correspondientes githubs para más información).'],
+      estado: 'En desarrollo.'
+    },
+    // Add more project objects here
+  ];
+
   const personDivRef = useRef(null);
+
   useEffect(() => {
     if (positionProyectos.x === 0) {
       setTimeout(() => {
-        updatePositionHome({ x: -20 })
-        updatePositionHall({ x: 0 })
-        navigate('/hall')
+        updatePositionHome({ x: -20 });
+        updatePositionHall({ x: 0 });
+        navigate('/hall');
       }, 300);
     }
-    window.scrollTo(personDivRef)
-  }, [positionProyectos])
-  const handdleInfo = () => {
-    if (info) {
-      setInfo(false)
-    } else { setInfo(true) }
-  }
-  useEffect(() => {
+    window.scrollTo(personDivRef);
+  }, [positionProyectos]);
 
-  }, [info])
   return (
     <div className='proyectos-container'>
       <div className='pas-sup'>
@@ -45,36 +66,38 @@ const Proyectos = () => {
           <div className="puerta-hall"></div>
         </div>
         <div className='cuadros-container'>
-
-          <div className={info ? 'infoExtended' : 'info'}>
-            <div className='inf-sup'><p>{paginasWeb.nombre}</p>
-              <button className='infor-btn' onClick={handdleInfo}>
-                {info ? '-' : '+'}
-              </button>
+          {projects.map((project, index) => (
+            <div key={index} className={activeProjectIndex === index ? 'infoExtended' : 'info'}>
+              <div className='inf-sup'>
+                <p>{project.nombre}</p>
+                <button className='infor-btn' onClick={() => setActiveProjectIndex(activeProjectIndex === index ? null : index)}>
+                  {activeProjectIndex === index ? '-' : '+'}
+                </button>
+              </div>
+              {activeProjectIndex === index && (
+                <ul>
+                  <li>Descripción: {project.descripcion}</li>
+                 {project.gitHubBackend === 'Sin backend' ? ( <li>Backend: {project.gitHubBackend}</li>):( <li>Backend: <a href={project.gitHubBackend} target='_blank' rel='noopener noreferrer'>{project.gitHubBackend}</a></li>)}
+                  <li>Frontend: <a href={project.gitHubFrontend} target='_blank' rel='noopener noreferrer'> {project.gitHubFrontend}</a></li>
+                 {project.urlProyecto === 'Próximamente' ? (<li>Url del proyecto:{project.urlProyecto}</li>) : ( <li>Url del proyecto: <a href={project.urlProyecto} target='_blank' rel='noopener noreferrer'> {project.urlProyecto}</a></li>)}
+                  <li>Principales tecnologías: {project.tecnologías}</li>
+                  <li>Estado del proyecto: {project.estado}</li>
+                </ul>
+              )}
+              <div className="cuadro">
+                <div className="marco">
+                  <img src={project.img} alt="proyecto" className='proyecto-imgInd' width={'150px'} height={'150px'}/>
+                </div>
+              </div>
             </div>
-            {info && <ul>
-              <li>Descripción:{paginasWeb.descripcion}</li>
-              <li>Backend: {paginasWeb.gitHubBackend}</li>
-              <li>Frontend: <a href='https://github.com/adrianMartinezTeba/paginaPersonal' target='blank'>{paginasWeb.gitHubFrontend}</a></li>
-              <li>Url del proyecto:<a href="https://portfolio-adrianmartinezteba.vercel.app/" target='blank'> {paginasWeb.urlProyecto}</a></li>
-              <li>Tecnologías: {paginasWeb.tecnologías}</li>
-            </ul>}
-          </div>
-          <div className="cuadro">
-            <div className="marco">
-              <img src={paginasWeb.img} alt="proyecto" className='proyecto-imgInd' />
-            </div>
-          </div>
-
+          ))}
         </div>
         <div ref={personDivRef} className='person-div' style={{ '--position-x': positionProyectos.x }}>
-          <Person />
-        </div>
+            <Person />
+          </div>
       </div>
       <div className="pas-inf"></div>
-
     </div>
-
   );
 };
 
--- src/components/RedirectComponent/RedirectComponent.jsx
+++ src/components/RedirectComponent/RedirectComponent.jsx
@@ -6,19 +6,19 @@ const RedirectComponent = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const handleResize = () => {
-      const isMobile = window.innerWidth < 768; // Puedes ajustar el ancho de pantalla para determinar qué consideras "móvil"
-      if (isMobile) {
-        navigate('/biografia');
-      }else{
-        navigate('/home')
-      }
-    };
+    // const handleResize = () => {
+      // const isMobile = window.innerWidth < 768; // Puedes ajustar el ancho de pantalla para determinar qué consideras "móvil"
+    //   if (isMobile) {
+    //     navigate('/biografia');
+    //   }else{
+    //     navigate('/home')
+    //   }
+    // };
 
-    handleResize();
-    window.addEventListener('resize', handleResize);
+    // handleResize();
+    // window.addEventListener('resize', handleResize);
 
-    return () => window.removeEventListener('resize', handleResize);
+    // return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return null; // No renderizamos nada, ya que esta es solo una redirección antes de la renderización real.
